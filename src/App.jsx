94f75c887340aa92f93e4c91ad5198771774206d
Mohd Faziel <mohdfazel969@gmail.com>
2024-12-05 01:24:02 +0530

    added functionality to app.jsx i.e load already login user

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import authService from "./appwrite/auth";
+import  {login,logout} from './store/features/authSlice';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 function App() {
-  return (
-    <>
-     <h1>Blog App</h1>
-    </>
+  //loader to be shown while we fetch the data from the server
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser().then((userData)=>
+    {
+      if(userData)
+      {
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout());
+      }
+    }
+    ).finally(()=>
+    {
+      setLoading(false);
+    })
+  }, [])
+  return loading ? (null) : (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+
+    </div>
   )
 }
 
